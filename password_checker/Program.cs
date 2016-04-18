1c7905732d57ee971b9de94fbe266c7b49ab0f38
Rehan Ahmed <ahmeshaf.rehan@gmail.com>
2016-04-18 11:23:59 +0530

    Checking in the skeleton of the password checker

--- password_checker/Program.cs
+++ password_checker/Program.cs
@@ -10,6 +10,53 @@ namespace PasswordChecker
     {
         static void Main(string[] args)
         {
+            var password_file = "password-list.txt";
+            var passwordDictionary = GetPasswordDictionary(password_file);
+            var inputPassword = Console.ReadLine();
+            if (PasswordInDictionary(inputPassword, passwordDictionary))
+            {
+                Console.WriteLine("Password Exists in Dictionary!");
+            }
+            else if (inputPassword.Length > 16)
+            {
+                Console.WriteLine("Password Legnth greater than 16!");
+            }
+            else
+            {
+                Console.WriteLine(GetStrongness(inputPassword));
+            }
+        }
+
+        /// <summary>
+        /// Get the strongness
+        /// </summary>
+        /// <param name="inputPassword"></param>
+        /// <returns>NORMAL, HIGH, STRONG</returns>
+        private static string GetStrongness(string inputPassword)
+        {
+            var specialCharString = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ";
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Check whether the password exist in dictionary.
+        /// </summary>
+        /// <param name="inputPassword"></param>
+        /// <param name="passwordDictionary"></param>
+        /// <returns></returns>
+        private static bool PasswordInDictionary(string inputPassword, Dictionary<int, List<string>> passwordDictionary)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Store the passwords as a dictionary where the key(bucket) is the length of the password
+        /// </summary>
+        /// <param name="password_file"></param>
+        /// <returns></returns>
+        private static Dictionary<int,List<String>> GetPasswordDictionary(string password_file)
+        {
+            throw new NotImplementedException();
         }
     }
 }
