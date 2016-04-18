dd159c95e626cbd1064104f4f54eb0163f308da6
Supreetha K <supreetha.k@hp.com>
2016-04-18 16:23:22 +0530

    Implemented matrix transpose

--- matrix_transpose/Program.cs
+++ matrix_transpose/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,42 @@ namespace matrix_transpose
     {
         static void Main(string[] args)
         {
+            var input_file = "Input_4000_6000.bin";
+            var output = "Output_6000_4000.bin";
+            byte[] fileBytes = System.IO.File.ReadAllBytes(input_file);
+            byte[] outputBytes = GetTranspose(fileBytes);
+
+            
+            File.WriteAllBytes(output, outputBytes);
+        }
+
+        private static byte[] GetTranspose(byte[] fileBytes)
+        {
+            var outputBytes = new byte[24000000];
+            int colCount = 6000;
+            int rowCount = 4000;
+            
+            int index = 0;
+            for (int row = 0; row < colCount; row++)
+            {
+                for (int col = 0; col < rowCount; col++)
+                {
+                    var kCnt = row + colCount * col;
+                    outputBytes[index++] = fileBytes[kCnt];
+                }
+            }
+
+            //Parallel.For(0, colCount,
+            //       index_i =>
+            //       {
+            //           Parallel.For(0, rowCount,
+            //              index_j =>
+            //              {
+            //                  outputBytes[index++] = fileBytes[index_i + colCount * index_j];
+            //              });
+            //       });
+
+            return outputBytes;
         }
     }
 }
